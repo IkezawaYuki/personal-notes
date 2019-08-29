4c117d42269c71918edac1bd5845bd929478cff6
yikezawa <y.ikezawa93@gmail.com>
2019-08-29 22:42:30 +0900

    first commit

---
+++ src/components/Edit.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="edit container">
+
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: "Edit",
+  data(){
+    return{
+      msg: "main"
+    }
+  }
+}
+</script>
+
+
+<style scoped>
+
+</style>
